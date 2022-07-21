1bce0e1186382f3304dba1ab4ba111edf8ca7913
Angelo Guzzo <angelo.guzzo83@gmail.com>
2022-07-21 16:17:39 +0200

    implementata schermata elenco piatti

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/categories_screen.dart';
+import 'package:flutter_complete_guide/screens/category_meals_screen.dart';
+
+import 'screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData(
+      primarySwatch: Colors.pink,
+      canvasColor: Color.fromRGBO(255, 254, 229, 1),
+      fontFamily: 'Raleway',
+      textTheme: ThemeData.light().textTheme.copyWith(
+          bodySmall: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          bodyMedium: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1),
+          ),
+          titleSmall: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold,
+          )),
+    );
     return MaterialApp(
       title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: CategoriesScreen(),
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+        secondary: Colors.amber,
+      )),
+      routes: {
+        '/': (context) => CategoriesScreen(),
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen(),
+      },
     );
   }
 }
